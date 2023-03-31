69c662e3fec41a699cd6fb1ebfb8600b520bf041
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-03-31 00:52:15 +0000

    Attempt 1 to fix linter errors

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <?php echo "<h1>Discount Calculator in PHP</h1>" ?>
 
     <!--Brief explanation of webpage-->
-    <?php echo "<p> We all love discounts on our purchases. Buying things on sale allows us to save money and it opens up space for the newest arrivials to come in. However, sometimes it can be hard to mentally figure out how much you will end up paying, with the discounts inpalce. Below you can find a dicount calculator that has been coded in php, which will determine your final price for you! So you can be confident in your purchases, just like this man is below.<p>"
+    <?php echo "<p> We all love discounts on our purchases. Buying things on sale allows us to save money and it opens up space for the newest arrivials to come in. However, sometimes it can be hard to mentally figure out how much you will end up paying, with the discounts inpalce. Below you can find a dicount calculator that has been coded in php, which will determine your final price for you! So you can be confident in your purchases, just like this man is below.<p>" ?>
 
 
     <!--Image-->
